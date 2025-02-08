840f313b2e92e57e167cf24caa70ce8acbfc99dd
Abid <abidroid1@gmail.com>
2025-02-08 11:52:45 +0500

    dashboard screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,7 @@
+import 'package:akti4_firebase_todo/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'add_task_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -10,6 +13,34 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return const AddTaskScreen();
+          }));
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          title: const Text('DASHBOARD'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return const ProfileScreen();
+                  }));
+                },
+                icon: Icon(Icons.person)),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.logout),
+            ),
+          ]),
+      body: const Center(),
+    );
   }
 }
