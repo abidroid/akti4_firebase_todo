50ac17c96d4556e5640f9bbd61af40a2b0572d08
Abid <abidroid1@gmail.com>
2025-02-09 11:04:39 +0500

    add task profile screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -8,8 +8,54 @@ class AddTaskScreen extends StatefulWidget {
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
+
+  var taskC = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        title: const Text('ADD TASK'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: taskC,
+              decoration: InputDecoration(
+                hintText: 'Task Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+
+                String taskName = taskC.text.trim();
+
+                if( taskName.isEmpty){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Task Name is required'),
+                    ),
+                  );
+                }
+
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+
+    );
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -10,6 +10,14 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        title: const Text('PROFILE'),
+      ),
+
+      body: const Placeholder(),
+    );
   }
 }
